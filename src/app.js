5bef90212ce14c3b616900d9aa43a56666a9e214
João Victor Lima <joaovictorlimapinto@gmail.com>
2020-08-18 01:24:01 -0400

    Add repository delete

--- src/app.js
+++ src/app.js
@@ -50,7 +50,18 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const hasRepository = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (hasRepository === -1) {
+    return response.status(400).json();
+  }
+
+  repositories.splice(hasRepository, 1);
+
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
